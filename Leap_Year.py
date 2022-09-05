37e08c70b66d8e53f5878e4765787d50bc161b0b
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-05 10:12:19 +0530

    Second Test of Divisible by 4

--- Leap_Year.py
+++ Leap_Year.py
@@ -3,5 +3,8 @@ def Leap_year1(year):
     if (year<0) :
         str="Year cannot be negative"
         return str
-    
+    if (year % 4 ==0):
+        return True
+    else:
+        return False
 
--- test_Leap_Year.py
+++ test_Leap_Year.py
@@ -4,7 +4,9 @@ class TestLeap_Year(unittest.TestCase):
     def test_NegativeYear(self):                 ## Checking the year is negative or not 
         stri="Year cannot be negative"
         self.assertEqual(Leap_Year.Leap_year1(-2000),stri) 
-
+    def test_divisble_by_four(self):             ## checking the year is divisible by 4
+        
+        self.assertEqual(Leap_Year.Leap_year1(2000),True)
         
         
 if __name__ == '__main__':
