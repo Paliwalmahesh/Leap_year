05235818f3abf3f5e7c3f946cfd84a925e976e45
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-05 10:17:55 +0530

    Final test and Code

--- Leap_Year.py
+++ Leap_Year.py
@@ -3,7 +3,9 @@ def Leap_year1(year):
     if (year<0) :
         str="Year cannot be negative"
         return str
-    if (year % 4 ==0):
+    if (year % 400 == 0) and (year % 100 == 0):
+        return True
+    elif (year % 4 ==0) and (year % 100 != 0):
         return True
     else:
         return False
--- test_Leap_Year.py
+++ test_Leap_Year.py
@@ -5,9 +5,13 @@ class TestLeap_Year(unittest.TestCase):
         stri="Year cannot be negative"
         self.assertEqual(Leap_Year.Leap_year1(-2000),stri) 
     def test_divisble_by_four(self):             ## checking the year is divisible by 4
-        
         self.assertEqual(Leap_Year.Leap_year1(2000),True)
-        
+        self.assertEqual(Leap_Year.Leap_year1(2001),False)
+    def test_Divisible_by_4_not_by_100(self):    ## checking the year is divisible by 4 but not by 100
+        self.assertEqual(Leap_Year.Leap_year1(2016),True)
+    def test_Divisible_by_100_but_not_400(self): ## checking the year divisible by 100 but not by 400
+         self.assertEqual(Leap_Year.Leap_year1(1900),False)
+
         
 if __name__ == '__main__':
     unittest.main()
